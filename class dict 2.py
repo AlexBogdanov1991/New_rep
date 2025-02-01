785594b871c405fe32f889504907cd22d83ff241
BogdanovALTX <sir.bogdanov2015@yandex.ru>
2025-02-01 12:44:55 +0300

    Dict 2

---
+++ class dict 2.py
@@ -0,0 +1,49 @@
+class MyDict:
+    def __init__(self):
+        self.data = []
+
+    def __getitem__(self, key):
+        for item in self.data:
+            if item[0] == key:
+                return item[1]
+        return None
+
+    def __setitem__(self, key, value):
+        for i in range(len(self.data)):
+            if self.data[i][0] == key:
+                self.data[i] = (key, value)
+                return
+        self.data.append((key, value))
+
+    def __delitem__(self, key):
+        for i in range(len(self.data)):
+            if self.data[i][0] == key:
+                del self.data[i]
+                return
+
+    def keys(self):
+        return [item[0] for item in self.data]
+
+    def values(self):
+        return [item[1] for item in self.data]
+
+    def items(self):
+        return [(item[0], item[1]) for item in self.data]
+
+    def __str__(self):
+        return str(dict(self.data))
+
+    def __contains__(self, key):
+        for item in self.data:
+            if item[0] == key:
+                return True
+        return False
+
+my_dict = MyDict()
+my_dict['name'] = 'Alice'
+my_dict['age'] = 30
+print(my_dict['name'])
+print('city' in my_dict)
+del my_dict['age']
+print(my_dict.keys())
+print(my_dict.values())
