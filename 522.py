fc0ad8a21a309d17595a2d7a2ca866ccd8a7b525
BogdanovALTX <sir.bogdanov2015@yandex.ru>
2025-02-11 12:24:38 +0300

    1

---
+++ 522.py
@@ -0,0 +1,31 @@
+import os
+
+dir_name = "Управление_файлами"
+os.makedirs(dir_name, exist_ok=True)
+
+file1_path = os.path.join(dir_name, "file1.txt")
+file2_path = os.path.join(dir_name, "file2.txt")
+
+with open(file1_path, 'w', encoding='utf-8') as file1:
+    file1.write("Это содержимое file1.")
+
+with open(file2_path, 'w', encoding='utf-8') as file2:
+    file2.write("Это содержимое file2.")
+
+print("Содержимое директории:")
+for item in os.listdir(dir_name):
+    print(item)
+
+os.remove(file2_path)
+
+subdir_name = os.path.join(dir_name, "Поддиректория")
+os.makedirs(subdir_name, exist_ok=True)
+
+new_file_path = os.path.join(subdir_name, "file1.txt")
+os.rename(file1_path, new_file_path)
+
+os.rmdir(subdir_name)
+os.rmdir(dir_name)
+
+print("Все операции выполнены успешно.")
+
