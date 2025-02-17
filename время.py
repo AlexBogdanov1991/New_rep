a5a44614e70d6d677fb84618178d703328668ad2
BogdanovALTX <sir.bogdanov2015@yandex.ru>
2025-02-17 11:53:51 +0300

    new

--- время.py
+++ время.py
@@ -1,4 +1,3 @@
-
 import datetime
 
 now = datetime.datetime.now()
@@ -16,3 +15,16 @@ if is_leap_year:
 else:
     print(f"Год {year} не является високосным.")
 
+
+from datetime import datetime
+
+user_input = input("Введите дату в формате 'год-месяц-день' (например, '2023-12-31'): ")
+target_date = datetime.strptime(user_input, '%Y-%m-%d')
+
+current_date = datetime.now()
+time_difference = target_date - current_date
+days_remaining = time_difference.days
+hours_remaining, seconds = divmod(time_difference.seconds, 3600)
+minutes_remaining, _ = divmod(seconds, 60)
+
+print(f"Осталось: {days_remaining} дней, {hours_remaining} часов и {minutes_remaining} минут.")
