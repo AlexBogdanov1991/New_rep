ed1cb7a21fa90d25a9304c406597fcf91dd47c49
BogdanovALTX <sir.bogdanov2015@yandex.ru>
2025-02-18 12:36:19 +0300

    new

---
+++ практика 2..py
@@ -0,0 +1,38 @@
+
+import random
+from collections import Counter
+
+random_list = [random.randint(1, 100) for _ in range(20)]
+counter = Counter(random_list)
+most_common = counter.most_common(3)
+
+print("Случайный список чисел:", random_list)
+print("Три наиболее часто встречающихся элемента:")
+for num, count in most_common:
+    print(f"Число {num} встречается {count} раз")  #задание 1
+
+
+
+from collections import defaultdict
+
+grouped_items = defaultdict(list)
+
+grouped_items['фрукты'].append('яблоко')
+grouped_items['фрукты'].append('банан')
+grouped_items['овощи'].append('морковь')
+grouped_items['фрукты'].append('апельсин')
+grouped_items['овощи'].append('картофель')   #Задание 3
+
+for category, items in grouped_items.items():
+    print(f"{category}: {items}")
+
+
+from collections import deque
+queue = deque([1, 2, 3, 4, 5, 6, 7,]) #задание 4
+queue.append(8)
+queue.appendleft(0)
+print(queue)
+
+queue.pop()
+queue.popleft()
+print(queue)
